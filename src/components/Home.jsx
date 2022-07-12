b0fb4fe3ceea29ba8c2827445841a34d76101954
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-07-12 16:04:26 +0100

    Added About component. Complex CSS. Name given for react scroll. flex col used to match things up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <p className='text-pink-600 text-2xl'>Hey. My name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Paul Chamberlain</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Junior Developer </h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>Previously a Bookseller for 11 years, currently I am focused on using my problem solving skills and passion for learning to create full-stack applications.  </p>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>A recent graduate from the School of Code, I look forward to progressing my journey in tech and learning new skills. At the minute, I am focused on React and CSS and plan to broaden my knowledge of back-end tech-stacks next.   </p>
             <div>
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Projects 
                 
