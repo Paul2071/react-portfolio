7a65a94d5753194731ab44b1c74af272e754eca9
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-07-12 15:42:11 +0100

    Home componenet done. Contains my name, info about me and a button with a few hover effects. Will review colour scheme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,19 @@ const Home = () => {
         {/* conatiner */}
 
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+            <p className='text-pink-600 text-2xl'>Hey. My name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Paul Chamberlain</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Junior Developer </h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>Previously a Bookseller for 11 years, currently I am focused on using my problem solving skills and passion for learning to create full-stack applications.  </p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Projects 
+                
+                <span className='group-hover:rotate-90 duration-300'>
 
+                <HiArrowNarrowRight className='ml-3 ' />
+                </span>
+                </button>
+            </div>
         </div>
 
     </div>
