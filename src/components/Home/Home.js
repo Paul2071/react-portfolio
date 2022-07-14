d6b5bf47482b328ea8bcd4025e12bbba4d86f18b
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-07-14 21:40:44 +0100

    tweaked the wording of the home page. I should stop. Going to rebuild then post on linkedin

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
             <p className='text-[#56dcc3] text-2xl'>Hey. My name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#9cdbcf] '>Paul Chamberlain</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#88b08d]'>I am a Junior Developer </h2>
-            <p className='text-[#3cf73c] py-4 max-w-[700px] text-justify'>A recent graduate from the School of Code, I look forward to continuing my journey in tech and learning new skills. At the minute, I am working on cementing my understanding of React and CSS and plan to broaden my knowledge of backend to  enable building fullstack applications.   </p>
+            <p className='text-[#3cf73c] py-4 max-w-[700px] text-justify'>A member of the most recent School of Code cohort, I look forward to continuing my journey in tech and learning new skills. At the minute, I am working on cementing my understanding of React and CSS and plan to broaden my knowledge of backend to  enable building fullstack applications.   </p>
             <div>
                 <button className='text-white group border-2 px-6 py-3 my-2 rounded-2xl flex items-center hover:bg-[#56dcc3] hover:border-[#56dcc3] hover:text-[#0a0a0a]'>Scroll Down For More
                 
