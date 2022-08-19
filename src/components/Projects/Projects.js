aa9c93e6b3e63b80577bdac0e88737a49f6cec70
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-08-19 14:50:46 +0100

    added BOK project to Projects section, changed favicon. Need to deploy something else

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Proficiency from   "../../assets/Proficiency.png"
 import WeatherApp from "../../assets/WeatherApp.png"
+import BoKApp from "../../assets/BoKApp.png"
 
 const Work = () => {
   return (
@@ -10,28 +11,28 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Projects
           </p>
-          <p className="py-6">My most recent projects</p>
+          <p className="py-6">My most recent projects. Hover over each image for a "Demo" and "Code" link</p>
         </div>
 
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${Proficiency})` }}
+            style={{ backgroundImage: `url(${BoKApp})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
             <span className="text-2xl font-bold bg-gray-700 p-4 rounded-lg tracking-wider">
-                Proficiency Tracker
+            Book Storage App 
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://hackson5bok.vercel.app" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-gray-700 text-white font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/SchoolOfCode/final-project_back-end-hackson5" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-gray-700 text-white font-bold text-lg">
                     Code
                   </button>
